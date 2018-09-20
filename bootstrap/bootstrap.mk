7943bcbd9f0d8a06ee9dcf403e4c44090605fae6
Yonghua Zheng <yh.zheng@yuneec.com>
2018-09-20 17:55:40 +0800

    bootstrap: rename atmel-sama5d2 to sama5d2-xplained

--- bootstrap/bootstrap.mk
+++ bootstrap/bootstrap.mk
@@ -1,11 +1,11 @@
 $(call inherit-product, $(PRODUCT_DIR)bootstrap_armv7a_neon.mk)
 
 PRODUCT_COPY_FILES += \
-	$(PRODUCT_DIR)atmel-sama5d2/init.bootstrap.rc:root/init.bootstrap.rc
+	$(PRODUCT_DIR)sama5d2-xplained/init.bootstrap.rc:root/init.bootstrap.rc
 
 # product override
 PRODUCT_NAME := bootstrap
-PRODUCT_DEVICE := sama5d2
+PRODUCT_DEVICE := sama5d2-xplained
 PRODUCT_BRAND := Lampoo
 PRODUCT_MODEL := Bootstrap on sama5d2
 
