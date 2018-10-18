e3adec976d9fe5d911acee68127f89ad1c93a5b6
Yonghua Zheng <yh.zheng@yuneec.com>
2018-10-18 13:50:25 +0800

    bootstrap: add includes.mk
    This is to prevent Android build script from traversing in every subdir
    in the root directory looking for Android.mk, by setting up a link
    in manifest like Android.mk -> vendor/lampoo/bootstrap/includes.mk.

---
+++ bootstrap/includes.mk
@@ -0,0 +1,17 @@
+-include abi/cpp/Android.mk
+-include bionic/Android.mk
+-include build/libs/host/Android.mk
+-include build/target/board/Android.mk
+-include build/tools/Android.mk
+-include external/compiler-rt/Android.mk
+-include external/e2fsprogs/Android.mk
+-include external/gcc-demangle/Android.mk
+-include external/mksh/Android.mk
+-include external/openssl/Android.mk
+-include external/stlport/Android.mk
+-include external/zlib/Android.mk
+-include frameworks/native/libs/binder/Android.mk
+-include hardware/libhardware/Android.mk
+-include system/core/Android.mk
+-include system/extras/Android.mk
+-include vendor/yuneec/jacobian/Android.mk
